2797e8347202b7dc74097478c62a42a8ee8c74c9
Airat Karimov <frigesty@gmail.com>
2025-04-04 16:54:16 +0300

    update Credentials

--- src/test/java/ru/frigesty/data/Credentials.java
+++ src/test/java/ru/frigesty/data/Credentials.java
@@ -1,6 +1,6 @@
 package ru.frigesty.data;
 
 public class Credentials {
-    public static final String LOGIN = System.getProperty("userName");;
+    public static final String LOGIN = System.getProperty("userName");
     public static final String PASSWORD = System.getProperty("password");
 }
