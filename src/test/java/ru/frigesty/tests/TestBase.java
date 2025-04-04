0c8195e78b97fd5e3ec305259429820cac1a96d7
Airat Karimov <frigesty@gmail.com>
2025-04-04 16:19:17 +0300

    Edit TestBase

--- src/test/java/ru/frigesty/tests/TestBase.java
+++ src/test/java/ru/frigesty/tests/TestBase.java
@@ -4,21 +4,48 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import io.restassured.RestAssured;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import ru.frigesty.config.WebDriverConfig;
 import ru.frigesty.helpers.Attach;
+
+import java.util.Map;
 
 import static com.codeborne.selenide.Selenide.*;
 
 public class TestBase {
 
+    private static final WebDriverConfig webDriverConfig =
+            ConfigFactory.create(WebDriverConfig.class, System.getProperties());
+
     @BeforeAll
     static void setUpBrowserConfiguration() {
-        Configuration.browserSize = "1920x1080";
-        Configuration.pageLoadStrategy = "eager";
-        Configuration.baseUrl = "https://myshows.me";
-        RestAssured.baseURI = "https://myshows.me";
+
+        configureBrowser();
+        if (webDriverConfig.isRemote()) {
+            configureRemote();
+        }
+    }
+
+    private static void configureBrowser() {
+        Configuration.browser = webDriverConfig.browser();
+        Configuration.browserVersion = webDriverConfig.browserVersion();
+        Configuration.browserSize = webDriverConfig.browserSize();
+        Configuration.pageLoadStrategy = webDriverConfig.loadStrategy();
+        Configuration.baseUrl = webDriverConfig.baseUrl();
+        RestAssured.baseURI = webDriverConfig.baseUrl();
+    }
+
+    private static void configureRemote() {
+        Configuration.remote = webDriverConfig.remoteUrl();
+        Configuration.browserCapabilities = new DesiredCapabilities();
+        Configuration.browserCapabilities.setCapability("selenoid:options", Map.of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
     }
 
     @BeforeEach
